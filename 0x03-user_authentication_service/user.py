37aa8fcfefb822e13ff35629517647a21cd4d97b
KofiFloki <jeremiah.martey@yahoo.com>
2023-11-14 18:33:18 +0000

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,6 +3,8 @@
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from db import DB
+from user import User
 
 
 Base = declarative_base()
@@ -17,3 +19,11 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+my_db = DB()
+
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
+
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
