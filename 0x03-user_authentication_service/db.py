7abe50517f2babf44d5422328101173151f378b5
KofiFloki <jeremiah.martey@yahoo.com>
2023-11-14 20:08:08 +0000

    create, find, update user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""DB module.
+"""
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class.
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object.
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+    
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ save the user to the database """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds a user in the db based on the keyword argument """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ updates user attributes using find_user_by to get the user """
+        if user_id is None:
+            return None
+        try:
+            user = self.find_user_by(id=user_id)
+        except Exception:
+            raise ValueError
+        for k, v in kwargs.items():
+            if not hasattr(user, k):
+                raise ValueError
+            setattr(user, k, v)
+        try:
+            self._session.commit()
+        except InvalidRequestError:
+            raise ValueError
